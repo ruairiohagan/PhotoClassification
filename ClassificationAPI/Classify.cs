dcf84114a4f6438bf0df1a4fb7d1a56d3bc3493e
Ruairi <ruairi.o'hagan@decipha.com.au>
2025-01-16 16:02:16 +1100

    Now tries to retrieve the date taken for the Image date, or failing that the last write time.

--- ClassificationAPI/Classify.cs
+++ ClassificationAPI/Classify.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace ClassificationAPI
 {
@@ -12,6 +14,57 @@ namespace ClassificationAPI
     /// </summary>
     public class Classify
     {
+
+        #pragma warning disable CA1416 // Validate platform compatibility
+        /// <summary>
+        /// Returns the date taken property of the image path provided or the last write time if that can't be obtained
+        /// </summary>
+        private static DateTime GetDataTaken(string imagePath)
+        {
+            if (!File.Exists(imagePath)) return DateTime.MinValue;
+
+            using (Image i = System.Drawing.Image.FromFile(imagePath))
+            {
+                {
+                    DateTime? dateTaken = null;
+
+                    PropertyItem? prop = null;
+                    try
+                    {
+                        prop = i.GetPropertyItem(36867); // The magic number that returns the date taken field
+                    }
+                    catch
+                    {
+                        prop = null;
+                    }
+
+                    if (prop != null)
+                    {
+                        // Convert the value to a readable DateTime format
+                        string dateTakenStr = System.Text.Encoding.ASCII.GetString(prop?.Value ?? []).Trim();
+
+                        if (dateTakenStr.Length > 19)
+                        {
+                            dateTakenStr = dateTakenStr.Substring(0, 19);
+                        }
+
+                        if (DateTime.TryParseExact(dateTakenStr, "yyyy:MM:dd HH:mm:ss", null, System.Globalization.DateTimeStyles.None, out DateTime parsedDT))
+                        {
+                            dateTaken = parsedDT;
+                        }
+                    }
+                    if (dateTaken == null)
+                    {
+                        // No Date Taken property found, just find the last write time instead
+                        FileInfo imageInfo = new FileInfo(imagePath);
+                        dateTaken = imageInfo.LastWriteTime;
+                    }
+
+                    return dateTaken ?? DateTime.MinValue;
+                }
+            }
+        }
+
         /// <summary>
         /// Classify all images in the given folder with the provided model name
         /// </summary>
@@ -37,7 +90,6 @@ namespace ClassificationAPI
             int curFileIndex = 0;
             foreach (string file in fileList)
             {
-                FileInfo imageInfo = new FileInfo(file);
                 string descriptorFile = Images.ImageFileDescriptorPath(file);
                 if (replace || !File.Exists(descriptorFile))
                 {
@@ -54,7 +106,7 @@ namespace ClassificationAPI
                     if (classRes.resultCode == System.Net.HttpStatusCode.OK)
                     {
                         File.WriteAllText(descriptorFile, classRes.description);
-                        ClassificationData.Images.UpsertImage(file, classRes.description, imageInfo.CreationTime, DateTime.Now, modelName, (int)elapsed.TotalMilliseconds);
+                        ClassificationData.Images.UpsertImage(file, classRes.description, GetDataTaken(file), DateTime.Now, modelName, (int)elapsed.TotalMilliseconds);
                         if (!progress(new ClassifyProgress() { 
                             Status = classRes.resultCode, 
                             Description = classRes.description, 
